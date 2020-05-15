824ec52ba3a7445f2b64cdc32ad284632a0dc086
Valentine <Allen.Valentine@disney.com>
2020-05-15 12:42:19 -0400

    Initial commit

---
+++ src/allen/learn/Main.java
@@ -0,0 +1,23 @@
+package allen.learn;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var employee = new Employee();
+        employee.setFirstName("Charlie");
+        employee.setLastName("Chocolate");
+        employee.setTitle("President");
+        employee.setPhoneNumber("222-222-2222");
+
+        var employee2 = new Employee();
+        employee2.setFirstName("Fred");
+        employee2.setLastName("Flintstone");
+        employee2.setTitle("Bedrock supervisor");
+
+
+        System.out.println("Name: " + employee.getFirstName() + " " + employee.getLastName());
+        System.out.println("Title: " + employee.getTitle());
+        System.out.println("Phone: " + employee.getPhoneNumber());
+    }
+}
